60f88ededd9f2b985daebda580aa929527e77854
Edwin Duenez <duenezedwin33@gmail.com>
2019-01-24 19:56:24 -0800

    Install body-parser

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 var app = express();
 const port = 3000;
 
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
 app.set("view engine", "ejs");
 
+// Index Route
 app.get("/", function(req, res) {
   res.render("index");
 });
 
-app.post("/", function(req, res) {});
+// Post Route
+app.post("/", function(req, res) {
+  var newTodo = req.body.newTodo;
+
+  res.render("index");
+});
 
 app.listen(port, function() {
   console.log("App listening on port 3000");
