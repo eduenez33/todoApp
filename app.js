544a503d6492491f9b0e31d1d7f8e0c90187ec39
Edwin Duenez <duenezedwin33@gmail.com>
2019-01-24 19:30:47 -0800

    Install EJS

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ var express = require("express");
 var app = express();
 const port = 3000;
 
+app.set("view engine", "ejs");
+
 app.get("/", function(req, res) {
-  res.send("Hello World");
+  res.render("index");
 });
 
-app.post("/", function(req, res) {
-  res.send("Got a POST request");
-});
+app.post("/", function(req, res) {});
 
 app.listen(port, function() {
   console.log("App listening on port 3000");
